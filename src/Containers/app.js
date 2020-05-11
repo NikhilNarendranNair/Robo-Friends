7f49e33f3f41d4a04e3ae3accad1adaa65369a0b
Nikhil Nair <nairnick@yahoo.com>
2020-05-11 19:43:27 +0530

    Fixed a small mistake in app.js file

--- src/Containers/app.js
+++ src/Containers/app.js
@@ -17,8 +17,7 @@ class App extends Component{
 
 	componentDidMount()
 	{
-		fetch('https://jsonplaceholder.typicode.com/users');
-		fetch('./app.json')
+		fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response=> response.json())
 		.then(users => this.setState({robots : users}));
 	}
