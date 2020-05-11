a22d4ef295ff07b0f842877f75ff9203209d12a0
Nikhil Nair <nairnick@yahoo.com>
2020-05-11 19:35:52 +0530

    Un-Commenting the URL for username, which was commented for try-catch

--- src/Containers/app.js
+++ src/Containers/app.js
@@ -17,7 +17,7 @@ class App extends Component{
 
 	componentDidMount()
 	{
-/*		fetch('https://jsonplaceholder.typicode.com/users')*/
+		fetch('https://jsonplaceholder.typicode.com/users');
 		fetch('./app.json')
 		.then(response=> response.json())
 		.then(users => this.setState({robots : users}));
